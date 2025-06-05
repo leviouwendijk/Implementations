897b082133291655bb54aafe47c60c0e45c4e8f9
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-06-05 17:33:07 +0200

    public init

--- Sources/Implementations/mailer/viewmodels/mailer-viewmodel.swift
+++ Sources/Implementations/mailer/viewmodels/mailer-viewmodel.swift
@@ -3,5 +3,7 @@ import Foundation
 public class MailerViewModel: ObservableObject {
     @Published public var mailerOutput: String = ""
     @Published public var sharedMailerCommandCopy: String = ""
+
+    public init() {}
 }
 
