6f8decda1922f9b40fe92be34a9b1d853a0d3c2e
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-06-05 17:12:20 +0200

    updated imports

--- Package.swift
+++ Package.swift
@@ -31,6 +31,10 @@ let package = Package(
             url: "https://github.com/leviouwendijk/Economics.git",
             branch: "master"
         ),
+        .package(
+            url: "https://github.com/leviouwendijk/Structures.git",
+            branch: "master"
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -42,6 +46,7 @@ let package = Package(
                 // .product(name: "ViewComponents", package: "ViewComponents"),
                 .product(name: "Interfaces", package: "Interfaces"),
                 .product(name: "Economics", package: "Economics"),
+                .product(name: "Structures", package: "Structures"),
             ],
             resources: [
                 .process("Resources")
@@ -55,6 +60,7 @@ let package = Package(
                 // .product(name: "ViewComponents", package: "ViewComponents"),
                 .product(name: "Interfaces", package: "Interfaces"),
                 .product(name: "Economics", package: "Economics"),
+                .product(name: "Structures", package: "Structures"),
             ]
         ),
     ]
