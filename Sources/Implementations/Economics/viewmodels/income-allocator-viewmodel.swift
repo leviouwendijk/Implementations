fb983703bb9d965909b04ba70db5580fbb59daf0
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-06-07 21:18:11 +0200

    display formatting

--- Sources/Implementations/Economics/viewmodels/income-allocator-viewmodel.swift
+++ Sources/Implementations/Economics/viewmodels/income-allocator-viewmodel.swift
@@ -54,7 +54,7 @@ public class IncomeAllocatorViewModel: ObservableObject {
         let allocator = IncomeAllocator(income: income, allocations: allocations)
         let summary = allocator.divide()
         allocationResults = summary.entries.map { entry in
-            String(format: "%@: %.2f", entry.allocation.account.rawValue, entry.result)
+            return "\(entry.allocation.account.rawValue): \(entry.result.display(decimals: 3))"
         }
     }
 
