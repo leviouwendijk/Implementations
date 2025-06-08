af491ac960ed16896acd033767ddeedf017ffcef
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-06-08 17:54:52 +0200

    catch the err in master viewmodel

--- Sources/Implementations/Economics/viewmodels/income-allocator-viewmodel.swift
+++ Sources/Implementations/Economics/viewmodels/income-allocator-viewmodel.swift
@@ -3,26 +3,38 @@ import Combine
 import Economics
 
 public class IncomeAllocatorViewModel: ObservableObject {
-    public var allocatorVm: IncomeAllocatorAccountsViewModel
+    public var allocatorVm: IncomeAllocatorAccountsViewModel?
     public var compounderVm: CompounderViewModel
 
     private var cancellables = Set<AnyCancellable>()
 
-    public init() throws {
+    @Published var errorMessage: String = ""
+
+    public init() {
         self.compounderVm = CompounderViewModel()
-        self.allocatorVm = try IncomeAllocatorAccountsViewModel()
+        do {
+            self.allocatorVm = try IncomeAllocatorAccountsViewModel()
+            bind()
+        } catch {
+            self.errorMessage = error.localizedDescription
+        }
 
-        allocatorVm.$selectedAccountAmount
-            .map { newValue in
-                return String(format: "%.2f", newValue)
-            }
-        .assign(to: \.monthlyInvestmentText, on: compounderVm)
+    }
+
+    public func bind() {
+        if let alloc = allocatorVm {
+            alloc.$selectedAccountAmount
+                .map { newValue in
+                    return String(format: "%.2f", newValue)
+                }
+            .assign(to: \.monthlyInvestmentText, on: compounderVm)
+                .store(in: &cancellables)
+
+                alloc.$incomeText
+                .sink { [weak self] _ in
+                    self?.compounderVm.calculate()
+                }
             .store(in: &cancellables)
-
-            allocatorVm.$incomeText
-            .sink { [weak self] _ in
-                self?.compounderVm.calculate()
-            }
-        .store(in: &cancellables)
+        }
     }
 }
