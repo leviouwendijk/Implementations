80ea2a84f74565d598a23d825949b6fe9d77fbb4
leviouwendijk <120993067+leviouwendijk@users.noreply.github.com>
2025-06-09 23:47:07 +0200

    adding boolean check and appending in mailercli

--- Sources/Implementations/Responder/viewmodels/responder-viewmodel.swift
+++ Sources/Implementations/Responder/viewmodels/responder-viewmodel.swift
@@ -303,6 +303,7 @@ public class ResponderViewModel: ObservableObject {
             availabilityJSON: try? weeklyScheduleVm.availabilityJSON(),
             appointmentsJSON: try? appointmentsQueue.jsonString(),
             needsAvailability: apiPathVm.endpointNeedsAvailabilityVariable,
+            isAppointmentRoute: (apiPathVm.selectedRoute == .appointment),
             stateVariables: stateVars
         )
         return try args.string(includeBinaryName)
